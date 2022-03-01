0e88836460007d4e03f5b8fc6fdd9319c50c5c36
Hayden Brown <haydenbrowncinci@gmail.com>
2022-03-01 10:35:10 -0500

    initial

---
+++ src/HelloWorldPackage/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorldPackage;
+
+public class HelloWorld {
+
+	/**
+	 * This is the main method.
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+	
+}
